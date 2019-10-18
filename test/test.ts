65d5711220102bd91910806d08e8e2f72fdf6353
Brett <brett.goulder@gmail.com>
2019-10-18 13:10:23 -0700

    Fix columns issue in tests

--- test/test.ts
+++ test/test.ts
@@ -2,5 +2,8 @@ import Nock from '@fancy-test/nock'
 import * as Test from '@oclif/test'
 export {expect} from '@oclif/test'
 
+process.stdout.columns = 80
+process.stderr.columns = 80
+
 export const test = Test.test
 .register('nock', Nock)
